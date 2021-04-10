8fd7a4fd84500d03c7f48a92459ca7652b717f6a
baranov-i <baranoffs.life@gmail.com>
2021-04-10 18:04:49 +0300

    Add Picasso lib Add the ability to load an image

--- app/src/main/java/by/baranovdev/homework/ImageLoaderActivity.kt
+++ app/src/main/java/by/baranovdev/homework/ImageLoaderActivity.kt
@@ -1,10 +1,12 @@
 package by.baranovdev.homework
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import com.squareup.picasso.Picasso
 
 class ImageLoaderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,8 +15,18 @@ class ImageLoaderActivity : AppCompatActivity() {
 
         val textEditor = findViewById<EditText>(R.id.url_editor)
         val loadButton = findViewById<Button>(R.id.button_load_image)
+        val backButton = findViewById<Button>(R.id.back_button)
         val image = findViewById<ImageView>(R.id.image_homework_1)
 
+        val intent = Intent(this, MainActivity::class.java)
+
+        loadButton.setOnClickListener {
+            val pic = Picasso.get().load(textEditor.text.toString()).into(image)
+        }
+
+        backButton.setOnClickListener {
+            startActivity(intent)
+        }
 
 
     }
