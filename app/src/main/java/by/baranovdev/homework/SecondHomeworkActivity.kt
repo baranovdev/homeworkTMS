51d5c75484b5138486a846b50948d54f92aad022
baranov-i <baranoffs.life@gmail.com>
2021-04-15 17:46:57 +0300

    Add 3 new activities with layouts for them

---
+++ app/src/main/java/by/baranovdev/homework/SecondHomeworkActivity.kt
@@ -0,0 +1,24 @@
+package by.baranovdev.homework
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class SecondHomeworkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_homework)
+
+        val buttons : Array<Button> = arrayOf(findViewById(R.id.button_flags),findViewById(R.id.button_animation))
+        val intentFirst = Intent(this, FlagsActivity::class.java)
+        val intentSecond = Intent(this, AnimationActivity::class.java)
+
+        buttons[0].setOnClickListener {
+            startActivity(intentFirst)
+        }
+        buttons[1].setOnClickListener {
+            startActivity(intentSecond)
+        }
+    }
+}
