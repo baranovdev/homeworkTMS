d012a778de09fb7eeb0f28202c02b2703136843d
baranov-i <baranoffs.life@gmail.com>
2021-04-25 20:34:56 +0300

    Initial Add new activity with layout

---
+++ app/src/main/java/by/baranovdev/homework/ThirdHomeworkActivity.kt
@@ -0,0 +1,11 @@
+package by.baranovdev.homework
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ThirdHomeworkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third_homework)
+    }
+}
